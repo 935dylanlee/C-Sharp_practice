b475f9d6de5d956c59148603095e8387f74a83f9
935dylanlee <935dylanlee@gmail.com>
2024-07-17 09:43:03 +0800

    c sharp main

---
+++ week1_CH2_work/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week1_CH2_work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.ReadKey();
+        }
+    }
+}
